134d9200e88fc198ca80fe693bd39c17a9efe85d
WUesBE <56186827+WUesBE@users.noreply.github.com>
2019-10-05 10:13:17 +0300

    Add files via upload
    petla for  robiaca piramide z gwiazdek

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace wykladzik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1, j= 1, n = 24;
+            while (i <= n)
+            {
+                j = 1;
+                while (j <= i)
+                {
+                    
+                    Console.Write("*");
+                    j++;
+                }
+                Console.WriteLine("");
+                ++i;
+            }
+        }
+    }
+}
